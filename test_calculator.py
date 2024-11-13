5cdf83a5ca294340145161c0194531f1d1740e96
Sawit Koseeyaumporn <121187906+Celesca@users.noreply.github.com>
2024-11-13 04:04:15 +0000

    create test multiply

--- test_calculator.py
+++ test_calculator.py
@@ -15,6 +15,12 @@ class TestCalculator(unittest.TestCase):
     def test_subtract(self):
         self.assertEqual(self.calc.subtract(2, 1), 1)
 
+    def test_subtract_2(self):
+        self.assertEqual(self.calc.subtract(0, 1), -1)
+
+    def test_multiply(self):
+        self.assertEqual(self.calc.multiply(2, 1), 2)
+
     # Add the following test methods to the TestCalculator class:
 
 if __name__ == '__main__':
